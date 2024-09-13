44709b1e590c51b590b6d09d1f7b1ef75cb78b18
Sunil Prajapati <sunil.prajapati@statrys.com>
2024-09-13 21:37:43 +0530

    useMemo explanation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import SplitScreen from "./SplitScreen";
+import Demo from "./exampleUseMemo/demo";
 
-const LeftHandComponent = ({ name }) => {
-  return <h1 style={{ backgroundColor: "ButtonFace" }}>Left hand {name}</h1>;
-};
-
-const RightHandComponent = ({ message }) => {
-  return <h2 style={{ backgroundColor: "green" }}>Right hand {message}</h2>;
-};
 function App() {
   return (
     <>
-      <SplitScreen leftWeight={1} rightWeight={3}>
-        <LeftHandComponent name="Developer" />
-        <RightHandComponent message="good for u!" />
-      </SplitScreen>
+      <Demo />
     </>
   );
 }
---
+++ src/exampleUseMemo/demo.tsx
@@ -0,0 +1,34 @@
+import { useMemo, useState } from "react";
+
+import { initialItems } from "./utils";
+
+interface DemoProps {}
+
+function Demo({}: DemoProps) {
+  const [count, setCount] = useState(0);
+  const [items] = useState(initialItems);
+
+  //case1.
+
+  // const selectedItem = useMemo(
+  //   () => items.find((item) => item.isSelected),
+  //   [items]
+  // );
+
+  //case2.
+
+  const selectedItem = useMemo(
+    () => items.find((item) => item.id === count),
+    [items, count]
+  );
+
+  return (
+    <div className="tutorial">
+      <h1>Count: {count}</h1>
+      <h1>Selected Item: {selectedItem?.id}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
+
+export default Demo;
